2610d11c7dc9a08b071b41ec9a7f94386bde8f03
William Chargin <wchargin@gmail.com>
2019-03-12 10:51:11 -0700

    [tmp] Graph: TypeScriptify graph-controls
    Summary:
    This is `git read-tree 3b7dfee710e5ea87635c4e754293a47ef95d1b58`, which
    comes from stephanwlee/control. Split into two commits so that Git picks
    up the diff.
    
    Test Plan:
    None intrinsic; refer to that commit.

--- tensorboard/plugins/graph/tf_graph_controls/tf-graph-controls.ts
+++ tensorboard/plugins/graph/tf_graph_controls/tf-graph-controls.ts
@@ -1,6 +1,4 @@
-/*
-@license
-Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,49 +11,126 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+==============================================================================*/
+namespace tf.graph.controls {
 
-(function() { // Private scope.
+interface DeviceNameExclude {
+  regex: RegExp,
+}
+
+const DEVICE_NAME_REGEX = /device:([^:]+:[0-9]+)$/;
+
 /**
  * Display only devices matching one of the following regex.
  */
-var DEVICE_NAMES_INCLUDE = [
+const DEVICE_NAMES_INCLUDE: DeviceNameExclude[] = [
   {
     // Don't include GPU stream, memcpy, etc. devices
-    regex: /device:[^:]+:[0-9]+$/,
+    regex: DEVICE_NAME_REGEX,
   }
 ];
+
+interface StatsDefaultOff {
+  regex: RegExp,
+  msg: string, // 'Excluded by default since...'
+}
 
 /**
  * Stats from device names that match these regexes will be disabled by default.
  * The user can still turn on a device by selecting the checkbox in the device list.
  */
-var DEVICE_STATS_DEFAULT_OFF = [
-//  {
-//    regex: //,
-//    msg: 'Excluded by default since...'
-//  }
-];
+const DEVICE_STATS_DEFAULT_OFF: StatsDefaultOff[] = [];
 
-/**
- * TODO(stephanwlee): Convert this to proper type when converting to TypeScript.
- * @typedef {{
- *   run: string,
- *   tag: ?string,
- *   type: tf.graph.SelectionType,
- * }}
- */
-const Selection = {};
+export interface Selection {
+  run: string,
+  tag: string|null,
+  type: tf.graph.SelectionType,
+}
+
+export interface DeviceForStats {
+  [key: string]: boolean;
+}
+
+// TODO(stephanwlee): Move this to tf-graph-dashboard
+export interface TagItem {
+  tag: string|null,
+  displayName: string,
+  conceptualGraph: boolean,
+  opGraph: boolean,
+  profile: boolean,
+}
+
+// TODO(stephanwlee): Move this to tf-graph-dashboard
+export interface RunItem {
+  name: string,
+  tags: TagItem[],
+}
+
+// TODO(stephanwlee): Move this to tf-graph-dashboard
+export interface Dataset extends Array<RunItem> {
+}
+
+interface CurrentDevice {
+  device: string,
+  suffix: string,
+  used: boolean,
+  ignoredMsg: string|null,
+}
+
+export enum ColorBy {
+  COMPUTE_TIME = 'compute_time',
+  MEMORY = 'memory',
+  STRUCTURE = 'structure',
+  XLA_CLUSTER = 'xla_cluster',
+  OP_COMPATIBILITY = 'op_compatibility',
+}
+
+interface ColorParams {
+  minValue: number,
+  maxValue: number,
+  // HEX value describing color.
+  startColor: string,
+  // HEX value describing color.
+  endColor: string,
+}
+
+interface DeviceColor {
+  device: string;
+  color: string;
+}
+
+interface XlaClusterColor {
+  xla_cluster: string;
+  color: string;
+}
+
+// TODO(stephanwlee) Move this to tf-graph.html when it becomes TypeScript.
+interface ColorByParams {
+  compute_time: ColorParams,
+  memory: ColorParams,
+  device: DeviceColor[],
+  xla_cluster: XlaClusterColor[],
+}
+
+const GRADIENT_COMPATIBLE_COLOR_BY: Set<ColorBy> = new Set([
+    ColorBy.COMPUTE_TIME, ColorBy.MEMORY]);
+
 
 Polymer({
   is: 'tf-graph-controls',
   properties: {
     // Public API.
+    /**
+     * @type {?tf.graph.proto.StepStats}
+     */
     stats: {
       value: null,
       type: Object,
-      observer: '_statsChanged'
+      observer: '_statsChanged',
     },
+    /**
+     * @type {?Object<string, boolean>}
+     */
     devicesForStats: {
       value: null,
       type: Object,
@@ -63,9 +138,12 @@ Polymer({
       // TODO(stephanwlee): Change readonly -> readOnly and fix the setter.
       readonly: true,
     },
+    /**
+     * @type {!tf.graph.controls.ColorBy}
+     */
     colorBy: {
       type: String,
-      value: 'structure',
+      value: ColorBy.STRUCTURE,
       notify: true,
     },
     colorByParams: {
@@ -79,6 +157,9 @@ Polymer({
       observer: '_datasetsChanged',
       value: () => [],
     },
+    /**
+     * @type {tf.graph.render.RenderGraphInfo}
+     */
     renderHierarchy: {
       type: Object,
       notify: true,
@@ -94,7 +175,7 @@ Polymer({
     },
     selectedFile: {
       type: Object,
-      notify: true
+      notify: true,
     },
     _selectedRunIndex: {
       type: Number,
@@ -115,31 +196,31 @@ Polymer({
     },
     selectedNode: {
       type: String,
-      notify: true
+      notify: true,
     },
     _currentDevices: {
       type: Array,
-      computed: '_getCurrentDevices(devicesForStats)'
+      computed: '_getCurrentDevices(devicesForStats)',
     },
     _currentDeviceParams: {
       type: Array,
-      computed: '_getCurrentDeviceParams(colorByParams)'
+      computed: '_getCurrentDeviceParams(colorByParams)',
     },
     _currentXlaClusterParams: {
       type: Array,
-      computed: '_getCurrentXlaClusterParams(colorByParams)'
+      computed: '_getCurrentXlaClusterParams(colorByParams)',
     },
     _currentGradientParams: {
       type: Object,
-      computed: '_getCurrentGradientParams(colorByParams, colorBy)'
+      computed: '_getCurrentGradientParams(colorByParams, colorBy)',
     },
     showSessionRunsDropdown: {
       type: Boolean,
-      value: true
+      value: true,
     },
     showUploadButton: {
       type: Boolean,
-      value: true
+      value: true,
     },
     // This stores whether the feature for showing health pills is enabled in the first place.
     healthPillsFeatureEnabled: Boolean,
@@ -154,20 +235,27 @@ Polymer({
       value: true,
     },
   },
+
   listeners: {
-    'trace-inputs.change': '_traceInputToggleChanged'
+    'trace-inputs.change': '_traceInputToggleChanged',
   },
-  _traceInputToggleChanged: function(event) {
+
+  _traceInputToggleChanged: function(event: Event): void {
     // Flip the state of the trace inputs flag.
-    this.renderHierarchy.traceInputs = event.target.active;
+    // Needs to be casted to PaperToggleButton but constructor is missing.
+    const PaperToggleButton: any = event.target;
+    this.renderHierarchy.traceInputs = PaperToggleButton.active;
     tf.graph.scene.node.traceInputs(this.renderHierarchy);
   },
-  _xlaClustersProvided: function(renderHierarchy) {
+
+  _xlaClustersProvided: function(
+      renderHierarchy: tf.graph.render.RenderGraphInfo|null) {
     return renderHierarchy &&
         renderHierarchy.hierarchy &&
         renderHierarchy.hierarchy.xlaClusters.length > 0;
   },
-  _statsChanged: function(stats) {
+
+  _statsChanged: function(stats: tf.graph.proto.StepStats): void {
     if (stats == null) {
       return;
     }
@@ -187,27 +275,32 @@ Polymer({
     });
     this.set('devicesForStats', devicesForStats);
   },
-  _getCurrentDevices: function(devicesForStats) {
-    var all_devices = _.map(this.stats && this.stats.dev_stats, function(d) {
-      return d.device;
-    });
-    var devices = _.filter(all_devices, function(d) {
-      return _.some(DEVICE_NAMES_INCLUDE, function(rule) {
-        return rule.regex.test(d);
+
+  _getCurrentDevices: function(
+      devicesForStats: DeviceForStats): CurrentDevice[] {
+
+    const stats: tf.graph.proto.StepStats|null = this.stats;
+    const devStats: tf.graph.proto.DevStat[] = stats ? stats.dev_stats : [];
+    const all_devices = devStats.map((d) => d.device);
+    const devices = all_devices.filter((deviceName) => {
+      return DEVICE_NAMES_INCLUDE.some((rule) => {
+        return rule.regex.test(deviceName);
       });
     });
     // Devices names can be long so we remove the longest common prefix
     // before showing the devices in a list.
-    var suffixes = tf.graph.util.removeCommonPrefix(devices);
+    const suffixes = tf.graph.util.removeCommonPrefix(devices);
     if (suffixes.length == 1) {
-      var found = suffixes[0].match(/device:([^:]+:[0-9]+)$/);
+      const found = suffixes[0].match(DEVICE_NAME_REGEX);
       if (found) {
         suffixes[0] = found[1];
       }
     }
-    return _.map(devices, function(device, i) {
-      var ignoredMsg = null;
-      _.each(DEVICE_STATS_DEFAULT_OFF, function(rule) {
+    return devices.map((device, i) => {
+      let ignoredMsg = null;
+      // TODO(stephanwlee): this should probably bail on the first match or
+      // do something useful with multiple rule.msgs.
+      DEVICE_STATS_DEFAULT_OFF.forEach((rule) => {
         if (rule.regex.test(device)) {
           ignoredMsg = rule.msg;
         }
@@ -216,93 +309,121 @@ Polymer({
         device: device,
         suffix: suffixes[i],
         used: devicesForStats[device],
-        ignoredMsg: ignoredMsg
+        ignoredMsg: ignoredMsg,
       };
     });
   },
-  _deviceCheckboxClicked: function(checkbox) {
+
+  _deviceCheckboxClicked: function(event: Event): void {
     // Update the device map.
-    var devicesForStats = _.extend({}, this.devicesForStats);
-    var device = checkbox.target.value;
-    if (checkbox.target.checked) {
+    const input = (event.target as HTMLInputElement);
+    const devicesForStats: DeviceForStats = Object.assign(
+        {}, this.devicesForStats);
+    const device = input.value;
+    if (input.checked) {
       devicesForStats[device] = true;
     } else {
       delete devicesForStats[device];
     }
     this.set('devicesForStats', devicesForStats);
   },
-  _numTags: function(datasets, _selectedRunIndex) {
+
+  _numTags: function(datasets: Dataset, _selectedRunIndex: number): number {
     return this._getTags(datasets, _selectedRunIndex).length;
   },
-  _getTags: function(datasets, _selectedRunIndex) {
+
+  _getTags: function(
+      datasets: Dataset, _selectedRunIndex: number): TagItem[] {
+
     if (!datasets || !datasets[_selectedRunIndex]) {
       return [];
     }
     return datasets[_selectedRunIndex].tags;
   },
-  fit: function() {
-    document.querySelector('#scene').fit();
+
+  fit: function(): void {
+    // TODO(stephanwlee): document -> this. A WebComponent should not use
+    // document.querySelector. tf-graph renders tf-graph-scene#scene
+    // which means this component has an incomplete API and encapsulation is
+    // leaking in a bad way.
+    // Constructor is not exported and is not typed.
+    const tfGraphScene: any = document.querySelector('#scene');
+    tfGraphScene.fit();
   },
-  _isGradientColoring: function(stats, colorBy) {
-    return ["compute_time", "memory"].indexOf(colorBy) !== -1
-        && stats != null;
+
+  _isGradientColoring: function(
+      stats: tf.graph.proto.StepStats, colorBy: ColorBy): boolean {
+
+    return GRADIENT_COMPATIBLE_COLOR_BY.has(colorBy) &&
+        this._statsNotNull(stats);
   },
-  _equals: function(a, b) {
+
+  _equals: function(a: any, b: any): boolean {
     return a === b;
   },
-  _getCurrentDeviceParams: function(colorByParams) {
-    var deviceParams = _.filter(colorByParams.device, function(param) {
-      return _.some(DEVICE_NAMES_INCLUDE, function(rule) {
+
+  _getCurrentDeviceParams: function(
+      colorByParams: ColorByParams): DeviceColor[] {
+
+    const deviceParams = colorByParams.device.filter((param) => {
+      return DEVICE_NAMES_INCLUDE.some((rule) => {
         return rule.regex.test(param.device);
       });
     });
     // Remove common prefix and merge back corresponding color. If
     // there is only one device then remove everything up to "/device:".
-    var suffixes = tf.graph.util.removeCommonPrefix(
-            _.map(deviceParams, function(d) { return d.device; }));
+    const suffixes = tf.graph.util.removeCommonPrefix(
+        deviceParams.map((d) => d.device));
     if (suffixes.length == 1) {
-      var found = suffixes[0].match(/device:([^:]+:[0-9]+)$/);
+      var found = suffixes[0].match(DEVICE_NAME_REGEX);
       if (found) {
         suffixes[0] = found[1];
       }
     }
-    return _.map(deviceParams, function(d, i) {
-      return { device : suffixes[i], color : d.color };
+    return deviceParams.map((d, i) => {
+      return {device: suffixes[i], color: d.color};
     });
   },
-  _getCurrentXlaClusterParams: function(colorByParams) {
+
+  _getCurrentXlaClusterParams: function(
+      colorByParams: ColorByParams): XlaClusterColor[] {
     return colorByParams.xla_cluster;
   },
-  _getCurrentGradientParams: function(colorByParams, colorBy) {
+
+  _getCurrentGradientParams: function(
+      colorByParams: ColorByParams, colorBy: ColorBy): ColorParams|void {
+
     if (!this._isGradientColoring(this.stats, colorBy)) {
       return;
     }
-    var params = colorByParams[colorBy];
-    var minValue = params.minValue;
-    var maxValue = params.maxValue;
-    if (colorBy === 'memory') {
+    const params: ColorParams = colorByParams[colorBy];
+    let minValue = params.minValue;
+    let maxValue = params.maxValue;
+    if (colorBy === ColorBy.MEMORY) {
       minValue = tf.graph.util.convertUnitsToHumanReadable(
           minValue, tf.graph.util.MEMORY_UNITS);
       maxValue = tf.graph.util.convertUnitsToHumanReadable(
           maxValue, tf.graph.util.MEMORY_UNITS);
-    } else if (colorBy === 'compute_time') {
+    } else if (colorBy === ColorBy.COMPUTE_TIME) {
       minValue = tf.graph.util.convertUnitsToHumanReadable(
           minValue, tf.graph.util.TIME_UNITS);
       maxValue = tf.graph.util.convertUnitsToHumanReadable(
           maxValue, tf.graph.util.TIME_UNITS);
     }
     return {
-      minValue: minValue,
-      maxValue: maxValue,
+      minValue,
+      maxValue,
       startColor: params.startColor,
-      endColor: params.endColor
+      endColor: params.endColor,
     };
   },
-  download: function() {
+
+  download: function(): void {
     this.$.graphdownload.click();
   },
-  _updateFileInput: function(e) {
-    const file = e.target.files[0];
+
+  _updateFileInput: function(e: Event): void {
+    const file = (e.target as HTMLInputElement).files[0];
     if (!file) return;
 
     // Strip off everything before the last "/" and strip off the file
@@ -319,13 +440,17 @@ Polymer({
     this._setDownloadFilename(filePath);
     this.set('selectedFile', e);
   },
-  _datasetsChanged: function(newDatasets, oldDatasets) {
+
+  _datasetsChanged: function(newDatasets: Dataset, oldDatasets: Dataset) {
     if (oldDatasets != null) {
       // Select the first dataset by default.
       this._selectedRunIndex = 0;
     }
   },
-  _computeSelection: function(datasets, _selectedRunIndex, _selectedTagIndex, _selectedGraphType) {
+
+  _computeSelection: function(datasets: Dataset, _selectedRunIndex: number,
+      _selectedTagIndex: number, _selectedGraphType: tf.graph.SelectionType) {
+
     if (!datasets[_selectedRunIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex]) {
       return null;
@@ -337,19 +462,23 @@ Polymer({
       type: _selectedGraphType,
     }
   },
-  _selectedRunIndexChanged: function(runIndex) {
+
+  _selectedRunIndexChanged: function(runIndex: number): void {
     if (!this.datasets) return;
     // Reset the states when user pick a different run.
-    this.colorBy = 'structure';
+    this.colorBy = ColorBy.STRUCTURE;
     this._selectedTagIndex = 0;
     this._selectedGraphType = this._getDefaultSelectionType();
     this.$['trace-inputs'].active = false; // Set trace input to off-state.
-    this._setDownloadFilename(this.datasets[runIndex] ? this.datasets[runIndex].name : '');
+    this._setDownloadFilename(
+        this.datasets[runIndex] ? this.datasets[runIndex].name : '');
   },
-  _selectedTagIndexChanged() {
+
+  _selectedTagIndexChanged(): void {
      this._selectedGraphType = this._getDefaultSelectionType();
   },
-  _getDefaultSelectionType() {
+
+  _getDefaultSelectionType(): tf.graph.SelectionType {
     const {
       datasets,
       _selectedRunIndex: run,
@@ -369,43 +498,60 @@ Polymer({
     }
     return tf.graph.SelectionType.OP_GRAPH;
   },
-  _getFile: function() {
+
+  _getFile: function(): void {
     this.$$("#file").click();
   },
-  _setDownloadFilename: function(name) {
+
+  _setDownloadFilename: function(name: string): void {
     this.$.graphdownload.setAttribute('download', name + '.png');
   },
-  _statsNotNull: function(stats) {
+
+  _statsNotNull: function(stats: tf.graph.proto.StepStats): boolean {
     return stats !== null;
   },
-  _toggleLegendOpen() {
+
+  _toggleLegendOpen(): void {
     this.set('_legendOpened', !this._legendOpened);
   },
-  _getToggleText(legendOpened) {
-    return legendOpened ? "Close legend." : "Expand legend.";
+
+  _getToggleText(legendOpened: boolean): string {
+    return legendOpened ? 'Close legend.' : 'Expand legend.';
   },
-  _getToggleLegendIcon(legendOpened) {
+
+  _getToggleLegendIcon(legendOpened: boolean): string {
     // This seems counter-intuitive, but actually makes sense because the
     // expand-more button points downwards, and the expand-less button points
     // upwards. For most collapsibles, this works because the collapsibles
     // expand in the downwards direction. This collapsible expands upwards
     // though, so we reverse the icons.
-    return legendOpened ? "expand-more" : "expand-less";
+    return legendOpened ? 'expand-more' : 'expand-less';
   },
-  _getSelectionOpGraphDisabled(datasets, _selectedRunIndex, _selectedTagIndex) {
+
+  _getSelectionOpGraphDisabled(datasets: Dataset,
+      _selectedRunIndex: number, _selectedTagIndex: number) {
+
     return !datasets[_selectedRunIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex].opGraph;
   },
-  _getSelectionProfileDisabled(datasets, _selectedRunIndex, _selectedTagIndex) {
+
+  _getSelectionProfileDisabled(datasets: Dataset,
+      _selectedRunIndex: number, _selectedTagIndex: number) {
+
     return !datasets[_selectedRunIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex].profile;
   },
-  _getSelectionConceptualGraphDisabled(datasets, _selectedRunIndex, _selectedTagIndex) {
+
+  _getSelectionConceptualGraphDisabled(datasets: Dataset,
+      _selectedRunIndex: number, _selectedTagIndex: number) {
+
     return !datasets[_selectedRunIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex] ||
         !datasets[_selectedRunIndex].tags[_selectedTagIndex].conceptualGraph;
   },
+
 });
-})(); // Closing private scope.
+
+}  // namespace tf.graph.controls
