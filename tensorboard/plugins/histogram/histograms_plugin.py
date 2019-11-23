30fcaab7db2db645bc633786a6652ed6ae2e1a15
TensorBoard Gardener <tensorboard-gardener@google.com>
2019-11-22 16:13:43 -0800

    black: reformat directory tensorboard/plugins/histogram

--- tensorboard/plugins/histogram/histograms_plugin.py
+++ tensorboard/plugins/histogram/histograms_plugin.py
@@ -38,57 +38,63 @@ from tensorboard.util import tensor_util
 
 
 class HistogramsPlugin(base_plugin.TBPlugin):
-  """Histograms Plugin for TensorBoard.
+    """Histograms Plugin for TensorBoard.
 
-  This supports both old-style summaries (created with TensorFlow ops
-  that output directly to the `histo` field of the proto) and new-style
-  summaries (as created by the `tensorboard.plugins.histogram.summary`
-  module).
-  """
-
-  plugin_name = metadata.PLUGIN_NAME
-
-  # Use a round number + 1 since sampling includes both start and end steps,
-  # so N+1 samples corresponds to dividing the step sequence into N intervals.
-  SAMPLE_SIZE = 51
-
-  def __init__(self, context):
-    """Instantiates HistogramsPlugin via TensorBoard core.
-
-    Args:
-      context: A base_plugin.TBContext instance.
+    This supports both old-style summaries (created with TensorFlow ops
+    that output directly to the `histo` field of the proto) and new-
+    style summaries (as created by the
+    `tensorboard.plugins.histogram.summary` module).
     """
-    self._db_connection_provider = context.db_connection_provider
-    self._multiplexer = context.multiplexer
 
-  def get_plugin_apps(self):
-    return {
-        '/histograms': self.histograms_route,
-        '/tags': self.tags_route,
-    }
+    plugin_name = metadata.PLUGIN_NAME
 
-  def is_active(self):
-    """This plugin is active iff any run has at least one histograms tag."""
-    if self._db_connection_provider:
-      # The plugin is active if one relevant tag can be found in the database.
-      db = self._db_connection_provider()
-      cursor = db.execute('''
+    # Use a round number + 1 since sampling includes both start and end steps,
+    # so N+1 samples corresponds to dividing the step sequence into N intervals.
+    SAMPLE_SIZE = 51
+
+    def __init__(self, context):
+        """Instantiates HistogramsPlugin via TensorBoard core.
+
+        Args:
+          context: A base_plugin.TBContext instance.
+        """
+        self._db_connection_provider = context.db_connection_provider
+        self._multiplexer = context.multiplexer
+
+    def get_plugin_apps(self):
+        return {
+            "/histograms": self.histograms_route,
+            "/tags": self.tags_route,
+        }
+
+    def is_active(self):
+        """This plugin is active iff any run has at least one histograms
+        tag."""
+        if self._db_connection_provider:
+            # The plugin is active if one relevant tag can be found in the database.
+            db = self._db_connection_provider()
+            cursor = db.execute(
+                """
         SELECT
           1
         FROM Tags
         WHERE Tags.plugin_name = ?
         LIMIT 1
-      ''', (metadata.PLUGIN_NAME,))
-      return bool(list(cursor))
+      """,
+                (metadata.PLUGIN_NAME,),
+            )
+            return bool(list(cursor))
 
-    return bool(self._multiplexer) and any(self.index_impl().values())
+        return bool(self._multiplexer) and any(self.index_impl().values())
 
-  def index_impl(self):
-    """Return {runName: {tagName: {displayName: ..., description: ...}}}."""
-    if self._db_connection_provider:
-      # Read tags from the database.
-      db = self._db_connection_provider()
-      cursor = db.execute('''
+    def index_impl(self):
+        """Return {runName: {tagName: {displayName: ..., description:
+        ...}}}."""
+        if self._db_connection_provider:
+            # Read tags from the database.
+            db = self._db_connection_provider()
+            cursor = db.execute(
+                """
         SELECT
           Tags.tag_name,
           Tags.display_name,
@@ -98,48 +104,57 @@ class HistogramsPlugin(base_plugin.TBPlugin):
           ON Tags.run_id = Runs.run_id
         WHERE
           Tags.plugin_name = ?
-      ''', (metadata.PLUGIN_NAME,))
-      result = collections.defaultdict(dict)
-      for row in cursor:
-        tag_name, display_name, run_name = row
-        result[run_name][tag_name] = {
-            'displayName': display_name,
-            # TODO(chihuahua): Populate the description. Currently, the tags
-            # table does not link with the description table.
-            'description': '',
-        }
-      return result
+      """,
+                (metadata.PLUGIN_NAME,),
+            )
+            result = collections.defaultdict(dict)
+            for row in cursor:
+                tag_name, display_name, run_name = row
+                result[run_name][tag_name] = {
+                    "displayName": display_name,
+                    # TODO(chihuahua): Populate the description. Currently, the tags
+                    # table does not link with the description table.
+                    "description": "",
+                }
+            return result
 
-    runs = self._multiplexer.Runs()
-    result = {run: {} for run in runs}
+        runs = self._multiplexer.Runs()
+        result = {run: {} for run in runs}
 
-    mapping = self._multiplexer.PluginRunToTagToContent(metadata.PLUGIN_NAME)
-    for (run, tag_to_content) in six.iteritems(mapping):
-      for (tag, content) in six.iteritems(tag_to_content):
-        content = metadata.parse_plugin_metadata(content)
-        summary_metadata = self._multiplexer.SummaryMetadata(run, tag)
-        result[run][tag] = {'displayName': summary_metadata.display_name,
-                            'description': plugin_util.markdown_to_safe_html(
-                                summary_metadata.summary_description)}
+        mapping = self._multiplexer.PluginRunToTagToContent(
+            metadata.PLUGIN_NAME
+        )
+        for (run, tag_to_content) in six.iteritems(mapping):
+            for (tag, content) in six.iteritems(tag_to_content):
+                content = metadata.parse_plugin_metadata(content)
+                summary_metadata = self._multiplexer.SummaryMetadata(run, tag)
+                result[run][tag] = {
+                    "displayName": summary_metadata.display_name,
+                    "description": plugin_util.markdown_to_safe_html(
+                        summary_metadata.summary_description
+                    ),
+                }
 
-    return result
+        return result
 
-  def frontend_metadata(self):
-    return base_plugin.FrontendMetadata(element_name='tf-histogram-dashboard')
+    def frontend_metadata(self):
+        return base_plugin.FrontendMetadata(
+            element_name="tf-histogram-dashboard"
+        )
 
-  def histograms_impl(self, tag, run, downsample_to=None):
-    """Result of the form `(body, mime_type)`, or `ValueError`.
+    def histograms_impl(self, tag, run, downsample_to=None):
+        """Result of the form `(body, mime_type)`, or `ValueError`.
 
-    At most `downsample_to` events will be returned. If this value is
-    `None`, then no downsampling will be performed.
-    """
-    if self._db_connection_provider:
-      # Serve data from the database.
-      db = self._db_connection_provider()
-      cursor = db.cursor()
-      # Prefetch the tag ID matching this run and tag.
-      cursor.execute(
-          '''
+        At most `downsample_to` events will be returned. If this value
+        is `None`, then no downsampling will be performed.
+        """
+        if self._db_connection_provider:
+            # Serve data from the database.
+            db = self._db_connection_provider()
+            cursor = db.cursor()
+            # Prefetch the tag ID matching this run and tag.
+            cursor.execute(
+                """
           SELECT
             tag_id
           FROM Tags
@@ -148,22 +163,23 @@ class HistogramsPlugin(base_plugin.TBPlugin):
             Runs.run_name = :run
             AND Tags.tag_name = :tag
             AND Tags.plugin_name = :plugin
-          ''',
-          {'run': run, 'tag': tag, 'plugin': metadata.PLUGIN_NAME})
-      row = cursor.fetchone()
-      if not row:
-        raise ValueError('No histogram tag %r for run %r' % (tag, run))
-      (tag_id,) = row
-      # Fetch tensor values, optionally with linear-spaced sampling by step.
-      # For steps ranging from s_min to s_max and sample size k, this query
-      # divides the range into k - 1 equal-sized intervals and returns the
-      # lowest step at or above each of the k interval boundaries (which always
-      # includes s_min and s_max, and may be fewer than k results if there are
-      # intervals where no steps are present). For contiguous steps the results
-      # can be formally expressed as the following:
-      #   [s_min + math.ceil(i / k * (s_max - s_min)) for i in range(0, k + 1)]
-      cursor.execute(
-          '''
+          """,
+                {"run": run, "tag": tag, "plugin": metadata.PLUGIN_NAME},
+            )
+            row = cursor.fetchone()
+            if not row:
+                raise ValueError("No histogram tag %r for run %r" % (tag, run))
+            (tag_id,) = row
+            # Fetch tensor values, optionally with linear-spaced sampling by step.
+            # For steps ranging from s_min to s_max and sample size k, this query
+            # divides the range into k - 1 equal-sized intervals and returns the
+            # lowest step at or above each of the k interval boundaries (which always
+            # includes s_min and s_max, and may be fewer than k results if there are
+            # intervals where no steps are present). For contiguous steps the results
+            # can be formally expressed as the following:
+            #   [s_min + math.ceil(i / k * (s_max - s_min)) for i in range(0, k + 1)]
+            cursor.execute(
+                """
           SELECT
             MIN(step) AS step,
             computed_time,
@@ -187,52 +203,66 @@ class HistogramsPlugin(base_plugin.TBPlugin):
             IFNULL(:sample_size - 1, max_step - min_step)
             * (step - min_step) / (max_step - min_step)
           ORDER BY step
-          ''',
-          {'tag_id': tag_id, 'sample_size': downsample_to})
-      events = [(computed_time, step, self._get_values(data, dtype, shape))
-                for step, computed_time, data, dtype, shape in cursor]
-    else:
-      # Serve data from events files.
-      try:
-        tensor_events = self._multiplexer.Tensors(run, tag)
-      except KeyError:
-        raise ValueError('No histogram tag %r for run %r' % (tag, run))
-      if downsample_to is not None and len(tensor_events) > downsample_to:
-        rand_indices = random.Random(0).sample(
-            six.moves.xrange(len(tensor_events)), downsample_to)
-        indices = sorted(rand_indices)
-        tensor_events = [tensor_events[i] for i in indices]
-      events = [[e.wall_time, e.step, tensor_util.make_ndarray(e.tensor_proto).tolist()]
-                for e in tensor_events]
-    return (events, 'application/json')
+          """,
+                {"tag_id": tag_id, "sample_size": downsample_to},
+            )
+            events = [
+                (computed_time, step, self._get_values(data, dtype, shape))
+                for step, computed_time, data, dtype, shape in cursor
+            ]
+        else:
+            # Serve data from events files.
+            try:
+                tensor_events = self._multiplexer.Tensors(run, tag)
+            except KeyError:
+                raise ValueError("No histogram tag %r for run %r" % (tag, run))
+            if downsample_to is not None and len(tensor_events) > downsample_to:
+                rand_indices = random.Random(0).sample(
+                    six.moves.xrange(len(tensor_events)), downsample_to
+                )
+                indices = sorted(rand_indices)
+                tensor_events = [tensor_events[i] for i in indices]
+            events = [
+                [
+                    e.wall_time,
+                    e.step,
+                    tensor_util.make_ndarray(e.tensor_proto).tolist(),
+                ]
+                for e in tensor_events
+            ]
+        return (events, "application/json")
 
-  def _get_values(self, data_blob, dtype_enum, shape_string):
-    """Obtains values for histogram data given blob and dtype enum.
-    Args:
-      data_blob: The blob obtained from the database.
-      dtype_enum: The enum representing the dtype.
-      shape_string: A comma-separated string of numbers denoting shape.
-    Returns:
-      The histogram values as a list served to the frontend.
-    """
-    buf = np.frombuffer(data_blob, dtype=tf.DType(dtype_enum).as_numpy_dtype)
-    return buf.reshape([int(i) for i in shape_string.split(',')]).tolist()
+    def _get_values(self, data_blob, dtype_enum, shape_string):
+        """Obtains values for histogram data given blob and dtype enum.
 
-  @wrappers.Request.application
-  def tags_route(self, request):
-    index = self.index_impl()
-    return http_util.Respond(request, index, 'application/json')
+        Args:
+          data_blob: The blob obtained from the database.
+          dtype_enum: The enum representing the dtype.
+          shape_string: A comma-separated string of numbers denoting shape.
+        Returns:
+          The histogram values as a list served to the frontend.
+        """
+        buf = np.frombuffer(
+            data_blob, dtype=tf.DType(dtype_enum).as_numpy_dtype
+        )
+        return buf.reshape([int(i) for i in shape_string.split(",")]).tolist()
 
-  @wrappers.Request.application
-  def histograms_route(self, request):
-    """Given a tag and single run, return array of histogram values."""
-    tag = request.args.get('tag')
-    run = request.args.get('run')
-    try:
-      (body, mime_type) = self.histograms_impl(
-          tag, run, downsample_to=self.SAMPLE_SIZE)
-      code = 200
-    except ValueError as e:
-      (body, mime_type) = (str(e), 'text/plain')
-      code = 400
-    return http_util.Respond(request, body, mime_type, code=code)
+    @wrappers.Request.application
+    def tags_route(self, request):
+        index = self.index_impl()
+        return http_util.Respond(request, index, "application/json")
+
+    @wrappers.Request.application
+    def histograms_route(self, request):
+        """Given a tag and single run, return array of histogram values."""
+        tag = request.args.get("tag")
+        run = request.args.get("run")
+        try:
+            (body, mime_type) = self.histograms_impl(
+                tag, run, downsample_to=self.SAMPLE_SIZE
+            )
+            code = 200
+        except ValueError as e:
+            (body, mime_type) = (str(e), "text/plain")
+            code = 400
+        return http_util.Respond(request, body, mime_type, code=code)
